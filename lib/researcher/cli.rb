78fec25b10e77a57c23fa1788d86a13f2d05a911
CiaraMaria Proia <proiacm@gmail.com>
2020-04-14 17:06:29 +0000

    added to cli.rb

--- lib/researcher/cli.rb
+++ lib/researcher/cli.rb
@@ -1,6 +1,16 @@
 class Cli 
   
   def start 
-    puts ""
-  end 
+    puts "Welcome to the COVID-19 statistics CLI!"
+    menu
+    input = gets.strip.downcase
+    Api.get_data(input)
+      while input != "exit"
+        if 
+    end
+  end
+  
+  def menu
+    puts "Type the name of a country to retrieve data or 'exit' to exit the program"
+  end
 end 
